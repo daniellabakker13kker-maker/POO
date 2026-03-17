296b21cca8f7efa493105357578d911ce45c7b09
daniellabakker13kker-maker <daniellabakker13kker@gmail.com>
2026-03-17 20:21:35 +0000

    deu certo agora?

--- Aula2/exemplos.py
+++ Aula2/exemplos.py
@@ -1,4 +1,4 @@
-l_num = list(map, int, input().split())
-par = (n for n in l_num if n % 2 == 0)
+l_num = list(map(int, input().split()))
+pares = (n for n in l_num if n % 2 == 0)
 impares = (n for n in l_num if n % 2 != 0)
-print(f"Soma pares: {sum(pares)} || Soma impares: {sum(impares)}"
+print(f"Soma pares: {sum(pares)} || Soma impares: {sum(impares)}")
