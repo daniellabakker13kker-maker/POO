24979dc5e992eaeeaaedf8e7649b665ad0006251
daniellabakker13kker-maker <daniellabakker13kker@gmail.com>
2026-03-17 20:51:01 +0000

    questao 3 concluida

--- Aula2/exemplos.py
+++ Aula2/exemplos.py
@@ -5,16 +5,27 @@
 # QUESTÃO 1 ^^^^^^^^^^^^^^^^
 
 
-num = int(input())
-meses = ["janeiro", "feveireiro", "março", "abril", "junho", "julho", "maio", "agosto", "setembro", "outubro", "novembro", "dezembro"]
-trimestres = ["primeiro", "segundo", "terceiro", "quarto"]
 
-if num >= 0 and num <= 2:
-    tri = trimestres[0]
-elif num >= 3 and num <= 5:
-    tri = trimestres[1]
-elif num >= 6 and num <= 8:
-    tri = trimestres[2]
-else:
-    tri = trimestres[3]
-print(f"O mês de {meses[num-1]} é o do {tri} trimestre do ano")
+# num = int(input())
+# meses = ["janeiro", "feveireiro", "março", "abril", "junho", "julho", "maio", "agosto", "setembro", "outubro", "novembro", "dezembro"]
+# trimestres = ["primeiro", "segundo", "terceiro", "quarto"]
+
+# if num >= 0 and num <= 2:
+#     tri = trimestres[0]
+# elif num >= 3 and num <= 5:
+#     tri = trimestres[1]
+# elif num >= 6 and num <= 8:
+#     tri = trimestres[2]
+# else:
+#     tri = trimestres[3]
+# print(f"O mês de {meses[num-1]} é o do {tri} trimestre do ano")
+# QUESTÃO 2 ^^^^^^^^^^^^^^^^
+
+
+
+# l_num = list(map(int, input().split()))
+# l_num.sort()
+# print(f"Maior número: {l_num[3]} | Menor número: {l_num[0]} || Soma dos 2° maior/menor: {(l_num[1]+l_num[2])}")
+# QUESTÃO 3 ^^^^^^^^^^^^^^^^
+
+
