cf930d8d190d744c98a46884cb87c906c49c3b3c
daniellabakker13kker-maker <daniellabakker13kker@gmail.com>
2026-03-30 20:51:27 +0000

    codigo area

---
+++ Aula2/lista01PO.py
@@ -0,0 +1,12 @@
+# AREA E CIRCUFERENCIA
+# AEREA FORMULA: a = pi * r^2
+# CIRCUFERENCIA: c = 2 * pi * r
+
+
+class Circulo():
+    def __init__(self, raio):
+        self.raio =  raio
+        self.pi = 3
+    
+    def calculo_area(self):
+        area = pi*(r**2)
