e616a2e401e216e8116ee70376bc6b03e6a8f7ea
daniellabakker13kker-maker <daniellabakker13kker@gmail.com>
2026-03-31 17:46:53 +0000

    finalizado quest 3

--- lista01PO_QUESTAO3.py
+++ lista01PO_QUESTAO3.py
@@ -8,8 +8,13 @@ class Conta():
         self.saldo = saldo
 
     def deposito(self, dinheiro_dep):
-        saldo_novo = self.saldo + dinheiro_dep
-        print(f"
-                Titular: {self.nome} 
-                Numero C: {self.numero}
-                Saldo atual: {saldo_novo}")
+        self.saldo += dinheiro_dep
+        print(f"Titular: {self.nome} \nNumero C: {self.numero} \nSaldo atual: {self.saldo}")
+
+    def saque(self, dinheiro_remov):
+        self.saldo -= dinheiro_remov
+        print(f"Titular: {self.nome} \nNumero C: {self.numero} \nSaldo atual: {self.saldo}")
+
+
+cliente = Conta("Luz", 2223334, 500)
+cliente.saque(500)
