fb2a7ae7d7f0a022ff4bb004fd558c5eaa36333f
daniellabakker13kker-maker <daniellabakker13kker@gmail.com>
2026-03-31 22:14:08 +0000

    inicio lista 2

---
+++ Lista 2 POO/questão1_Circulo.py
@@ -0,0 +1,15 @@
+class Circulo:
+    def __init__(self, raio):
+        __self.raio = raio # type: ignore
+
+    def set_raio(self, novo_raio):
+        _Circulo__self.raio = novo_raio
+        print(__self.raio)
+
+    def get_raio
+
+# uma pra REDEFINIR RAIO
+# uma pra PRINT RAIO
+# uma pra CALCULAR AREA (float)
+# uma pra CALCULAR CIRCUNFERENCIA (float)
+
