ccbbf717a78cb2a242f181cee136a72fe589ad77
daniellabakker13kker-maker <daniellabakker13kker@gmail.com>
2026-03-31 22:42:28 +0000

    correções

--- Lista 2 POO/questão1_Circulo.py
+++ Lista 2 POO/questão1_Circulo.py
@@ -1,29 +1,29 @@
 class Circulo:
-    def __init__(self, raio):
-        self._raio = raio 
+    def __init__(self):
+        self._raio = 0.0
 
-    def set_raio(self, novo_raio):
-        self._raio = novo_raio
+    def set_raio(self, raio):
+        self._raio = raio
 
     def get_raio(self):
         print(self._raio)
 
     def calc_area(self):
-        area = 3,14 * (self._raio ** 2)
+        area = 3.14 * (self._raio ** 2)
         print(area)
     
     def calc_circunferencia(self):
-        circuferencia = 2 * 3,14 * self._raio
+        circuferencia = 2 * 3.14 * self._raio
         print(circuferencia)
 
 forma1 = Circulo(2)
-forma1.get_raio
-forma1.calc_area
-forma1.calc_circunferencia
+forma1.get_raio()
+forma1.calc_area()
+forma1.calc_circunferencia()
 forma1.set_raio(5)
-forma1.get_raio
-forma1.calc_area
-forma1.calc_circunferencia
+forma1.get_raio()
+forma1.calc_area()
+forma1.calc_circunferencia()
 
 
 
